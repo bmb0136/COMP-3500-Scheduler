cad434dbbdde6e74b0ecd17d4bab3505ce3846f1
Brandon Buckley <brandonmb0136@gmail.com>
2025-04-11 16:37:07 +0000

    Remove other code

--- src/main.c
+++ src/main.c
@@ -1,8 +1,4 @@
 #include "config.h"
-#include "taskqueue.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 
 int main(int argc, char** argv) {
   struct config_t *config = config_parse(argc, argv);
@@ -10,12 +6,6 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  struct taskqueue_t* queue = taskqueue_create();
-  for (int i = 0; i < config->numTasks; i++) {
-    taskqueue_push(queue, config->tasks[i]);
-  }
-
   config_destroy(config);
-  taskqueue_destroy(queue);
   return 0;
 }
