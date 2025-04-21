5faee9baf9b0718906f003e43657b143c4f33667
Brandon Buckley <brandonmb0136@gmail.com>
2025-04-21 19:33:35 +0000

    Update SRTF to use taskheap for wait queue

--- src/policy_srtf.c
+++ src/policy_srtf.c
@@ -9,9 +9,9 @@ struct policy_t policy_srtf_create() {
   return (struct policy_t){.init = &init, .run = &run, .destroy = &destroy};
 }
 
-static struct taskheap_t *heap;
+static struct taskheap_t *heap, *wait;
 
-static int compare(struct task_t *a, struct task_t *b) {
+static int cmpByBurst(struct task_t *a, struct task_t *b) {
   int x = a->burstTime - b->burstTime;
   if (x == 0) {
     x = a->startTime - b->startTime;
@@ -19,29 +19,40 @@ static int compare(struct task_t *a, struct task_t *b) {
   return x;
 }
 
-static void init() { heap = taskheap_create(&compare); }
+static int cmpByStart(struct task_t *a, struct task_t *b) {
+  int x = a->startTime - b->startTime;
+  if (x == 0) {
+    x = a->pid - b->pid;
+  }
+  return x;
+}
 
-static void destroy() { taskheap_destroy(heap); }
+static void init() {
+  heap = taskheap_create(&cmpByBurst);
+  wait = taskheap_create(&cmpByStart);
+}
+
+static void destroy() {
+  taskheap_destroy(heap);
+  taskheap_destroy(wait);
+}
 
 static void run(struct pinput_t input, pemit_fp emit) {
   unsigned int time = 0;
 
+  for (size_t i = 0; i < input.numTasks; i++) {
+    taskheap_push(wait, input.futureTasks[i]);
+  }
+
   while (1) {
-    // Add tasks to ready "queue"
-    for (size_t i = 0; i < input.numTasks; i++) {
-      struct task_t t = input.futureTasks[i];
-      if (time >= t.startTime) {
-        taskheap_push(heap, t);
-        for (size_t j = i + 1; j < input.numTasks; j++) {
-          input.futureTasks[j - 1] = input.futureTasks[j];
-        }
-        input.numTasks--;
-        i--;
-      }
+    // Add tasks to ready queue
+    while (wait->count > 0 && time >= wait->items[0].startTime) {
+      taskheap_push(heap, wait->items[0]);
+      taskheap_pop(wait, NULL);
     }
 
     if (heap->count == 0) {
-      if (input.numTasks == 0) {
+      if (wait->count == 0) {
         emit((struct schedevent_t){.time = time, .type = SE_DONE});
         return;
       }
@@ -58,7 +69,7 @@ static void run(struct pinput_t input, pemit_fp emit) {
       taskheap_pop(heap, NULL);
       current = heap->items[0];
       if (heap->count == 0) {
-        if (input.numTasks == 0) {
+        if (wait->count == 0) {
           emit((struct schedevent_t){.time = time, .type = SE_DONE});
           return;
         }
