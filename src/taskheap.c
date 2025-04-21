661fd060cabbeaccd64cb19829c5e8973c9d589c
Brandon Buckley <brandonmb0136@gmail.com>
2025-04-21 18:01:56 +0000

    Fix ties and add custom compare function to task heap

--- src/taskheap.c
+++ src/taskheap.c
@@ -27,13 +27,18 @@ static void swap(struct taskheap_t *heap, size_t a, size_t b) {
   pidmap_update(heap->pidmap, heap->items[b].pid, &heap->items[b]);
 }
 
-struct taskheap_t *taskheap_create() {
+static int compare(struct taskheap_t *heap, int a, int b) {
+  return heap->comparator(&heap->items[a], &heap->items[b]);
+}
+
+struct taskheap_t *taskheap_create(thcmp_fp comparator) {
   struct taskheap_t *heap =
       (struct taskheap_t *)malloc(sizeof(struct taskheap_t));
   heap->count = 0;
   heap->capacity = 4;
   heap->items = (struct task_t *)malloc(heap->capacity * sizeof(struct task_t));
   heap->pidmap = pidmap_create();
+  heap->comparator = comparator;
   return heap;
 }
 
@@ -60,12 +65,35 @@ void taskheap_push(struct taskheap_t *heap, struct task_t task) {
   size_t i = heap->count++;
   heap->items[i] = task;
 
-  while (i >= 0 && parent(i) >= 0 && heap->items[i].burstTime < heap->items[parent(i)].burstTime) {
+  while (i >= 0 && parent(i) >= 0 && compare(heap, i, parent(i)) < 0) {
     swap(heap, i, parent(i));
     i = parent(i);
   }
 
   pidmap_add(heap->pidmap, &heap->items[i]);
+}
+
+static void heapifyDown(struct taskheap_t *heap, int start) {
+  size_t i = start;
+  while (i < heap->count) {
+    int L = left(i);
+    int R = right(i);
+    int cl = compare(heap, i, L) > 0;
+    int cr = compare(heap, i, R) > 0;
+    char tiebreaker = 1;
+    if (cl > 0 && cr > 0) {
+      tiebreaker = compare(heap, L, R) < 0;
+    }
+    if (L < heap->count && cl && tiebreaker) {
+      swap(heap, i, L);
+      i = L;
+    } else if (R < heap->count && cr) {
+      swap(heap, i, R);
+      i = R;
+    } else {
+      break;
+    }
+  }
 }
 
 char taskheap_pop(struct taskheap_t *heap, struct task_t *output) {
@@ -78,23 +106,14 @@ char taskheap_pop(struct taskheap_t *heap, struct task_t *output) {
   }
 
   pidmap_remove(heap->pidmap, heap->items[0].pid);
-  swap(heap, 0, --heap->count);
 
-
-  size_t i = 0;
-  while (i < heap->count) {
-    int L = left(i);
-    int R = right(i);
-    if (L < heap->count && heap->items[i].burstTime > heap->items[L].burstTime) {
-      swap(heap, i, L);
-      i = L;
-    } else if (R < heap->count && heap->items[i].burstTime > heap->items[R].burstTime) {
-      swap(heap, i, R);
-      i = R;
-    } else {
-      break;
-    }
+  if (--heap->count == 0) {
+    return 1;
   }
+
+  swap(heap, 0, heap->count);
+
+  heapifyDown(heap, 0);
 
   return 1;
 }
@@ -110,18 +129,5 @@ void taskheap_updatekey(struct taskheap_t *heap, int pid, int key) {
   swap(heap, 0, offset);
   heap->items[0].burstTime = key;
 
-  size_t i = 0;
-  while (i < heap->count) {
-    int L = left(i);
-    int R = right(i);
-    if (L < heap->count && heap->items[i].burstTime > heap->items[L].burstTime) {
-      swap(heap, i, L);
-      i = L;
-    } else if (R < heap->count && heap->items[i].burstTime > heap->items[R].burstTime) {
-      swap(heap, i, R);
-      i = R;
-    } else {
-      break;
-    }
-  }
+  heapifyDown(heap, 0);
 }
--- src/taskheap.h
+++ src/taskheap.h
@@ -4,14 +4,17 @@
 #include "task.h"
 #include "pidmap.h"
 
+typedef int (*thcmp_fp)(struct task_t *a, struct task_t *b);
+
 struct taskheap_t {
   struct task_t *items;
   int capacity;
   int count;
   struct pidmap_t *pidmap;
+  thcmp_fp comparator;
 };
 
-struct taskheap_t *taskheap_create();
+struct taskheap_t *taskheap_create(thcmp_fp comparator);
 void taskheap_destroy(struct taskheap_t *heap);
 
 void taskheap_push(struct taskheap_t *heap, struct task_t task);
